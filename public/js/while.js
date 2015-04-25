77f13125c0377922a5ccca4814e12e52497f3020
Claire Osburn <claire.a.osburn@gmail.com>
2015-04-25 11:51:22 -0500

    initial commit

---
+++ public/js/while.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var i = 0;
+
+// while (i<32768) {
+// 	i = i*2;
+// 	console.log(i);
+// }
+
+var double = 1;
+
+while (i < 16) {
+	double = double*2;
+	console.log(double);
+	i++;
+}
