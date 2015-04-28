5ca99ddf62fd0385bf34812e1063cfafb9699fab
Claire Osburn <claire.a.osburn@gmail.com>
2015-04-27 20:50:07 -0500

    adding recursive solution

--- public/js/fizzbuzz.js
+++ public/js/fizzbuzz.js
@@ -1,7 +1,22 @@
 "use strict";
+// Fizz Buzz using a Loop
+//
+// for (var i=1; i<=100; i++) {
+// 	if (i % 3 == 0 && i % 5 == 0) {
+// 		console.log("fizzbuzz");
+// 	} else if (i % 3 == 0) {
+// 		console.log("fizz");
+// 	} else if (i % 5 == 0) {
+// 		console.log("buzz");
+// 	} else {
+// 		console.log(i);
+// 	}
+// }
 
-for (var i=1; i<=100; i++) {
-	if (i % 3 == 0 && i % 5 == 0) {
+// Fizz Buzz using Recursion
+//
+function fizzBuzz (i) {
+	if (i % 15 == 0) {
 		console.log("fizzbuzz");
 	} else if (i % 3 == 0) {
 		console.log("fizz");
@@ -10,4 +25,10 @@ for (var i=1; i<=100; i++) {
 	} else {
 		console.log(i);
 	}
+	i++;
+	if (i <= 100) {
+		fizzBuzz(i);
+	}
 }
+
+fizzBuzz (1);
