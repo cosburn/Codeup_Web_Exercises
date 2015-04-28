743476f8f8dda3fdced32682072dafbacc22d078
Claire Osburn <claire.a.osburn@gmail.com>
2015-04-28 18:26:53 -0500

    initial commit

---
+++ public/js/iterating_arrays.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// todo:
+// Create an array of 4 names using literal array notation, in a variable called 'names'.
+
+// todo:
+// Create a log statement that will log the number of elements in the names array.
+
+// todo:
+// Create log statements that will print each of the names array elements individually.
+
+(function(){
+	var catNames = ["Nina","Whiskers","Sophia","Skamps"];
+
+	console.log(catNames.length);
+
+	for (var i=0;i<catNames.length;i++){
+		console.log(catNames[i]);
+	}
+
+})();
