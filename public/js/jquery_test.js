7c89680e133c223c66db4c694a612aad8eb5e257
Claire Osburn <claire.a.osburn@gmail.com>
2015-05-12 20:28:34 -0500

    targeting by class and id

--- public/js/jquery_test.js
+++ public/js/jquery_test.js
@@ -1,5 +1,10 @@
 "use strict";
 
 $(document).ready(function() {
-	console.log("The DOM has finished loading! (but not the images!)");
+	$("#main-container")
+		.css("background-color","pink")
+		.css("color","#6f6d73")
+		.css("padding","30px");
+	$(".codeup").css("border","1px solid red").css("padding","5px");
 });
+
