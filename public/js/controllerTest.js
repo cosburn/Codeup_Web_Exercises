2d683b8d733a86d51708c5437583bec4690c0617
Claire Osburn <claire.a.osburn@gmail.com>
2015-06-29 21:14:09 -0500

    finished exercise, added some background color and button style

--- public/js/controllerTest.js
+++ public/js/controllerTest.js
@@ -5,15 +5,15 @@
 	app.controller("CartController", function() {
 		this.store = [
 			{ 
-				icon:   "http://placehold.it/100x100",
+				icon:   "http://placehold.it/50x50",
 				name:   "PANTS",
-				price:  "$30.99",
+				price:  30.99,
 				qty:    1
 			},
 			{
-				icon:   "http://placehold.it/100x100",
+				icon:   "http://placehold.it/50x50",
 				name:   "SHIRT",
-				price:  "$15.99",
+				price:  15.99,
 				qty:    1
 			}
 		];
@@ -22,25 +22,38 @@
 
 		this.addItem = function(item) {
 			var exists;
+
 			this.cart.forEach(function(e, i) {
-				if (e == item) {
+				if (e.name == item.name) {
 					exists = true;
 				};
 			});
 			if (exists == true) {
 				var that = this;
 				this.cart.forEach(function(e, i) {
-					if (e == item) {
+					if (e.name == item.name) {
 						that.cart[i].qty++;
 					};
 				});
 			} else {
 				this.cart.push(item);
-			};			
+			};		
 		};
 
+		this.getSubtotal = function() {
+			// loop over cart
+			var that = this;
+			var total = 0;
+			this.cart.forEach(function(element, index) {
+				// add up total
+				total += (element.price * element.qty);
+			});
+			// return that total
+			return total;
+		}
+
 		this.removeItem = function(index) {
-			this.cart[index].qty == 1;
+			this.cart[index].qty = 1;
 			this.cart.splice(index,1);
 			console.log(this.cart);
 		};
